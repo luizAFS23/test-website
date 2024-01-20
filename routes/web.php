4cb9f63f77c88decdba4490ac537fa5654b286fc
luizAFS23 <luizafs.1500@gmail.com>
2024-01-20 20:53:16 -0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [EventController::class, 'index']); /* index = mesmo "index" da função index do controller EventController*/
 Route::get('/events/create', [EventController::class, 'create']);
