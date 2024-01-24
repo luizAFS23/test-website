377d9a07aeed7b8572052fe59c6d4ae01086222e
luizAFS23 <luizafs.1500@gmail.com>
2024-01-23 22:15:08 -0300

    changes

--- database/migrations/2024_01_23_190931_create_events_table.php
+++ database/migrations/2024_01_23_190931_create_events_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
         });
     }
 
