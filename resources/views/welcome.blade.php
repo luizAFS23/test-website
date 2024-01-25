fb07957533741da5cbdad02ce18174ad98c02bff
luizAFS23 <luizafs.1500@gmail.com>
2024-01-25 16:11:28 -0300

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
             <div class="card col-md-3">
                 <img src="/img/large.png" alt="{{ $event -> title }}">
                 <div class="card-body">
-                    <p class="card-date">10/09/2020</p>´
+                    <p class="card-date">10/09/2020</p>
                     <h5 class="card-title">{{$event -> title}}</h5>
                     <p class="card-participants">X Participantes</p>
                     <a href="" class="btn btn-primary">Saber mais</a>
