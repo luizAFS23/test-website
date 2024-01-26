feead50dcf7f9a5fde8ba5e6e43390d2e8f43b90
luizAFS23 <luizafs.1500@gmail.com>
2024-01-26 19:20:31 -0300

    changes

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
 
-<h1>Crie um evento</h1>
+<div id="event-create-container" class="col-md-6 offset md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="">
+
+    </form>
+</div>
 
 @endsection
