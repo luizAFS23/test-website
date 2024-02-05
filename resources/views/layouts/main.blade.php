dda723883bfdb5e82695546889be396c4cc41548
luizAFS23 <luizafs.1500@gmail.com>
2024-02-05 10:30:10 -0300

    changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -26,12 +26,19 @@
                     <li class="nav-item">
                         <a href="/" class="nav-link">Eventos</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="/events/create" class="nav-link">Criar Eventos</a>
-                    </li>
                     @auth
                         <li class="nav-item">
                             <a href="/dashboard" class="nav-link">Meus Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/events/create" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <a href="/logout" class="nav-link" onclick="event.preventDefault();
+                                                                            this.closest('form').submit();">Sair</a>
+                            </form>
                         </li>
                     @endauth
                     @guest
