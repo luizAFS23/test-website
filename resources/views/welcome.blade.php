cdeaa9bfa9d081b1e93054acd3edb28029dab41a
luizAFS23 <luizafs.1500@gmail.com>
2024-02-05 10:11:35 -0300

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,12 @@
 </div>
 
 <div id="events-container" class="col-md-12">
-    <h2>Próximos Eventos</h2>
-    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @if($search)
+        <h2>Buscando por: {{$search}}</h2>
+    @else
+        <h2>Próximos Eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
     <div id="cards-container" class="row">
         @foreach($events as $event)
             <div class="card col-md-3">
@@ -36,7 +40,9 @@
                 </div>
             </div>
         @endforeach
-        @if(count($events) == 0)
+        @if(count($events) == 0 && $search)
+            <p>Não foi possível encontrar por nenhum evento com {{$search}}! <a href="/">Ver todos os eventos</a></p>
+        @elseif(count($events) == 0)
             <p>Não há eventos disponíveis!</p>
         @endif
     </div>
